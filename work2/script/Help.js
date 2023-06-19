bfedbf10d5c59e8572c3be6fc903e489967d8488
eh08060 <128441105+eh08060@users.noreply.github.com>
2023-06-20 07:33:47 +0900

    6666666

--- work2/script/Help.js
+++ work2/script/Help.js
@@ -1,10 +1,21 @@
 window.addEventListener("scroll", function () {
-  var element = document.querySelector(".scroll-text");
-  var position = element.getBoundingClientRect();
-
-  if (position.top < window.innerHeight && position.bottom >= 0) {
-    element.classList.add("show");
-  } else {
-    element.classList.remove("show");
-  }
+  scrollMotion();
 });
+
+function scrollMotion() {
+  const ele = document.querySelectorAll(".scroll-text");
+  let eleOffsetArr = [];
+
+  ele.forEach((e) => {
+    let eleOffset = e.getBoundingClientRect().top + window.pageYOffset - 600;
+    eleOffsetArr.push(eleOffset);
+    console.log(eleOffset);
+  });
+
+  eleOffsetArr.forEach((offset, idx) => {
+    ele[idx].style.cssText =
+      window.scrollY > offset
+        ? "opacity:1; transform: translateY(0);"
+        : "opacity:0; transform: translateY(10px);";
+  });
+}
